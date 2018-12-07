294cbfab9a7b25c218b34e783d0a0066565febf8
ssheridan17 <ssheridan17@users.noreply.github.com>
2018-12-07 10:18:41 -0500

    Uploaded Basic Pong Code

---
+++ PongPanel.java
@@ -0,0 +1,138 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.image.*; 
+import java.io.*; 
+import javax.imageio.*; 
+
+public class PongPanel extends JPanel{
+
+	// properties; 
+	// int intRectX = 40;
+	int intRectY = 50; 
+	int intRectRY = 50; 
+	boolean blnGoDown = false; 
+	boolean blnGoUp = false; 
+	int intCX = 248;
+	int intCY = 125;
+	int intLCounter = 0; 
+	int intRCounter = 0; 
+	JLabel thelabel; 
+	
+	// boolean statements for ball 
+	boolean blnCUp = true;
+	boolean blnCDown = false; 
+	boolean blnCLeft = false; 
+	boolean blnCRight = true; 
+
+	public void paintComponent(Graphics g){
+	// repaints background
+	
+		g.setColor(Color.BLACK); 
+		g.fillRect(0,0,500,250);
+		
+		// repaints left side of panel 
+		g.setColor(Color.WHITE); 
+		g.fillRect(250,0,5,500); 
+		
+		g.fillRect(40, intRectY, 10,30); 
+		
+		
+		
+		// adjusts location of left rectangle 
+		if(blnGoDown){
+			intRectY += 3; 
+		}
+		
+		if(blnGoUp){
+			intRectY -= 3; 
+		}
+		
+		// right hand rectangle 
+		g.fillRect(460, intRectRY, 10, 30); 
+		
+		//circle 
+		// oval (x, y, width, height)
+		g.setColor(Color.RED); 
+		g.fillOval(intCX,intCY, 10, 10); 
+		
+		
+	
+		
+		// set perimaters (x-axis)
+		if(intCX == 0){
+			intCX = 248; 
+		}else if(intCX == 500){
+			intCX = 248;
+		}
+		
+		// set y perimeters
+		if(intCY <= 0){
+			blnCUp = false; 
+			blnCDown = true; 
+			System.out.println("got here"); 
+		}else if(intCY >= 250){
+			blnCDown = false; 
+			blnCUp = true; 
+		// System.out.println("got here"); 
+		}
+		
+		// set up right paddle board
+		
+		if(intCX >450 && intCX > intCX - 10){
+		
+			if(intCY >= intRectRY-15 && intCY <= intRectRY+15){ 
+				blnCRight = false; 
+				blnCLeft = true; 
+			}
+		}
+		
+		// set up left paddle board
+		if(intCX < 50 && intCX > intCX -10){
+			if(intCY >= intRectY-15 && intCY <= intRectY+15){
+				blnCLeft = false; 
+				blnCRight = true; 
+			}
+		}
+		
+		
+		// set up if statements for boolean to make the ball shift and it's direction
+		if(blnCUp){
+			intCY-= 2;
+		}
+		
+		if(blnCDown){
+			intCY += 2;
+		}
+		
+		if(blnCLeft){
+			intCX-= 2;
+		}
+		
+		if(blnCRight){
+			intCX+= 2;
+		}
+		
+		
+		
+		if(intCX <40){
+			intRCounter = intRCounter + 1; 
+			System.out.println(intRCounter); 
+		}else if(intCX > 460){
+			intLCounter = intLCounter +1;
+			System.out.println(intLCounter); 
+		}
+		
+		
+		
+		
+	}
+
+
+// constructor 
+public PongPanel(){
+	super();
+}
+
+
+
+}
---
+++ pong.java
@@ -0,0 +1,128 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+
+public class pong implements ActionListener, KeyListener, MouseMotionListener, MouseListener{
+	// properties
+	PongPanel thepanel;
+	JFrame theframe; 
+	Timer thetimer;
+	JLabel thelabel;
+	
+
+	// methods
+	public void actionPerformed(ActionEvent evt){
+		if(evt.getSource() == thetimer){
+			thepanel.repaint(); 
+		}
+	}
+	
+	
+	
+	// left panel controlled with keys
+	public void keyReleased(KeyEvent evt){
+		if(evt.getKeyCode() == 38){
+			thepanel.blnGoUp = false;
+		}else if(evt.getKeyCode() == 40){
+			thepanel.blnGoDown = false; 
+		}
+	
+	}
+	
+	public void keyPressed(KeyEvent evt){
+		if(evt.getKeyCode() == 38){
+			thepanel.blnGoUp = true;
+		}else if(evt.getKeyCode() == 40){
+			thepanel.blnGoDown = true; 
+		}
+	
+	
+	}
+	
+	public void keyTyped(KeyEvent evt){
+	
+	}
+	
+	// right panel controlled with mouse
+	
+	// mouse stuff
+	public void mouseMoved(MouseEvent evt){ 
+		thepanel.intRectRY = evt.getY(); 
+	}
+	
+	public void mouseDragged(MouseEvent evt){
+	
+	}
+	
+	// mouse part twoooo!
+	
+	public void mouseExited(MouseEvent evt){
+	
+	}
+	
+	public void mouseEntered(MouseEvent evt){
+	
+	}
+	
+	public void mouseReleased(MouseEvent evt){
+	
+	}
+	
+	public void mousePressed(MouseEvent evt){
+		System.out.println("sheridan"); 
+	}
+
+	public void mouseClicked(MouseEvent evt){
+	
+	}
+	
+	
+	
+	
+	
+	
+	// constructors
+	
+	public pong(){
+	theframe = new JFrame("pong"); 
+	thepanel = new PongPanel();
+	thepanel.setLayout(null); 
+	thepanel.setPreferredSize(new Dimension(500,250)); 
+	theframe.addKeyListener(this); 
+	thepanel.addMouseMotionListener(this); 
+	thepanel.addMouseListener(this); 
+	
+	
+	// Labels
+	
+	/* add score
+	
+	thelabel = new JLabel(""+thepanel.intLCounter); 
+	thelabel.setLocation(120,20); 
+	thelabel.setPreferredSize(new Dimension(100,20)); 
+	thepanel.add(thelabel); 
+	
+	*/
+	
+	theframe.setContentPane(thepanel); 
+	theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	theframe.pack(); 
+	theframe.setVisible(true); 
+	
+	thetimer = new Timer(1000/60,this); 
+	thetimer.start(); 
+	
+
+	
+	}
+	
+	// main method 
+	
+	public static void main(String[] args){
+		new pong(); 
+	
+	}
+	
+	
+
+}
